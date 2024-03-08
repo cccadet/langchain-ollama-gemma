19ee73fdef29f4223508cda7aaf699d86c18141e
Cristian C Santos <perestra.ccds@gmail.com>
2024-03-07 22:20:58 -0300

    lanchain retrival-agent template test

---
+++ LangChain Agents with Open Source Models!/app/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+
+from retrieval_agent_fireworks import agent_executor as retrieval_agent_fireworks_chain
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+add_routes(app, retrieval_agent_fireworks_chain, path="/retrieval-agent-fireworks")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
